f5797c10829cc19bf3e3f0d45fa5b21787794b28
giuseppe.caliendo@gmail.com <giuseppe.caliendo@gmail.com@f1d95f5b-4939-44ee-a64d-5954261e7e9b>
2013-05-28 14:15:53 +0000

    Rimosso l'installatore IzPack a favore del sistema interno di gradle. Rimangono da verificare il manuale e il sistema di controllo degli aggiornamenti

--- trunk/src/main/java/it/greentone/GreenToneAppConfig.java
+++ trunk/src/main/java/it/greentone/GreenToneAppConfig.java
@@ -25,8 +25,7 @@ import org.springframework.transaction.PlatformTransactionManager;
  * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
  * details. You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
- * </code>
- * <br>
+ * </code> <br>
  * <br>
  * Bean di configurazione tra Datanucleus e Spring framework.
  * 
@@ -34,93 +33,87 @@ import org.springframework.transaction.PlatformTransactionManager;
  */
 @Configuration
 public class GreenToneAppConfig {
-	/**
-	 * Separatore di path di sistema
-	 */
-	public static final String FILE_SEPARATOR = System
-			.getProperty("file.separator");
-	/**
-	 * Path di base dove salavare i dati di GreenTone
-	 */
-	public static final String BASE_PATH = System.getProperty("user.home")
-			+ FILE_SEPARATOR + "GreenTone" + FILE_SEPARATOR;
-	/**
-	 * Path del file di configurazione di Spring Framework
-	 */
-	public static final String SPRING_CONFIG_LOCATION = "classpath:/it/greentone/resources/springAppConfig.xml";
-	/**
-	 * Path del file di configurazione di Datanucleus
-	 */
-	public static final String DATANUCLEUS_CONFIG_LOCATION = "/it/greentone/resources/datanucleus.properties";
-	/**
-	 * Path del repository dei report
-	 */
-	public static final String REPORTS_REPOSITORY = BASE_PATH + "report"
-			+ FILE_SEPARATOR;
-	/**
-	 * Path del repository dove salvare la copia dei file associati a dei
-	 * documenti
-	 */
-	public static final String DOCUMENTS_REPOSITORY = BASE_PATH + "documents"
-			+ FILE_SEPARATOR;
-	/**
-	 * Path dove risiede il manuale utente
-	 */
-	public static final String MANUAL_REPOSITORY = BASE_PATH + "manual"
-			+ FILE_SEPARATOR;
-	/**
-	 * Nome del file del manuale utente
-	 */
-	public static final String MANUAL_FILE_NAME = "GreenTone-ManualeUtente.pdf";
-	/** Locale applicativo */
-	public static final Locale APPLICATION_LOCALE = Locale.ITALY;
-	/** Pattern di default utilizzato per le date nell'applicazione */
-	public static final String DATE_PATTERN = "dd/MM/yy";
-	/** Nome del file di log */
-	public static final String LOG_PATH = BASE_PATH + "log.txt";
+    /**
+     * Separatore di path di sistema
+     */
+    public static final String FILE_SEPARATOR = System.getProperty("file.separator");
+    /**
+     * Path di base dove salavare i dati di GreenTone
+     */
+    public static final String BASE_PATH = System.getProperty("user.home") + FILE_SEPARATOR + "GreenTone"
+            + FILE_SEPARATOR;
+    /**
+     * Path del file di configurazione di Spring Framework
+     */
+    public static final String SPRING_CONFIG_LOCATION = "classpath:/conf/springAppConfig.xml";
+    /**
+     * Path del file di configurazione di Datanucleus
+     */
+    public static final String DATANUCLEUS_CONFIG_LOCATION = "/conf/datanucleus.properties";
+    /**
+     * Path del repository dei report
+     */
+    public static final String REPORTS_REPOSITORY = BASE_PATH + "report" + FILE_SEPARATOR;
+    /**
+     * Path del repository dove salvare la copia dei file associati a dei documenti
+     */
+    public static final String DOCUMENTS_REPOSITORY = BASE_PATH + "documents" + FILE_SEPARATOR;
+    /**
+     * Path dove risiede il manuale utente
+     */
+    public static final String MANUAL_REPOSITORY = BASE_PATH + "manual" + FILE_SEPARATOR;
+    /**
+     * Nome del file del manuale utente
+     */
+    public static final String MANUAL_FILE_NAME = "GreenTone-ManualeUtente.pdf";
+    /** Locale applicativo */
+    public static final Locale APPLICATION_LOCALE = Locale.ITALY;
+    /** Pattern di default utilizzato per le date nell'applicazione */
+    public static final String DATE_PATTERN = "dd/MM/yy";
+    /** Nome del file di log */
+    public static final String LOG_PATH = BASE_PATH + "log.txt";
 
-	/**
-	 * Resituisce la factory di gestione della persistenza.
-	 * 
-	 * @return la factory di gestione della persistenza
-	 */
-	@Bean
-	PersistenceManagerFactory pmf() {
+    /**
+     * Resituisce la factory di gestione della persistenza.
+     * 
+     * @return la factory di gestione della persistenza
+     */
+    @Bean
+    PersistenceManagerFactory pmf() {
 
-		final Properties defaults = new Properties();
-		try {
-			defaults.load(getClass().getResourceAsStream(
-					DATANUCLEUS_CONFIG_LOCATION));
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		final Properties props = new Properties(defaults);
-		final StringBuilder connectionURL = new StringBuilder();
-		connectionURL.append("jdbc:h2:");
-		/* il db è posizionato nella cartella di esecuzione di greentone */
-		final String storageDirPath = BASE_PATH + "db";
-		if (storageDirPath != null && storageDirPath.length() > 0) {
-			connectionURL.append(storageDirPath);
-			if (!storageDirPath.endsWith("/")) {
-				connectionURL.append("/");
-			}
-		}
-		new File(storageDirPath).mkdirs();
-		connectionURL.append("GT_CAT;DB_CLOSE_ON_EXIT=TRUE");
-		props.put("javax.jdo.option.ConnectionURL", connectionURL.toString());
-		props.put("javax.jdo.option.ConnectionUserName", "user");
-		props.put("javax.jdo.option.ConnectionPassword", "");
-		return JDOHelper.getPersistenceManagerFactory(props);
-	}
+        final Properties defaults = new Properties();
+        try {
+            defaults.load(getClass().getResourceAsStream(DATANUCLEUS_CONFIG_LOCATION));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        final Properties props = new Properties(defaults);
+        final StringBuilder connectionURL = new StringBuilder();
+        connectionURL.append("jdbc:h2:");
+        /* il db è posizionato nella cartella di esecuzione di greentone */
+        final String storageDirPath = BASE_PATH + "db";
+        if (storageDirPath != null && storageDirPath.length() > 0) {
+            connectionURL.append(storageDirPath);
+            if (!storageDirPath.endsWith("/")) {
+                connectionURL.append("/");
+            }
+        }
+        new File(storageDirPath).mkdirs();
+        connectionURL.append("GT_CAT;DB_CLOSE_ON_EXIT=TRUE");
+        props.put("javax.jdo.option.ConnectionURL", connectionURL.toString());
+        props.put("javax.jdo.option.ConnectionUserName", "user");
+        props.put("javax.jdo.option.ConnectionPassword", "");
+        return JDOHelper.getPersistenceManagerFactory(props);
+    }
 
-	/**
-	 * Restituisce il manager delle transazioni.
-	 * 
-	 * @return il manager delle transazioni
-	 */
-	@Bean
-	public PlatformTransactionManager txManager() {
-		return new JdoTransactionManager(pmf());
-	}
+    /**
+     * Restituisce il manager delle transazioni.
+     * 
+     * @return il manager delle transazioni
+     */
+    @Bean
+    public PlatformTransactionManager txManager() {
+        return new JdoTransactionManager(pmf());
+    }
 
 }
